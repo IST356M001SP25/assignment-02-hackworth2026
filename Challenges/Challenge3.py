0d48f8839d036f0f49361696dcabf1521a0f03d2
hackworth2026 <Ahhackwo@syr.edu>
2025-01-30 14:28:44 -0500

    Modification to code

--- Challenges/Challenge3.py
+++ Challenges/Challenge3.py
@@ -1,4 +1,13 @@
 colors = []
 
 while True:
-    color = input("Enter a color or enter 'quit' to quit: ")
+    colors = input("Enter a color or enter 'quit' to quit: ")
+    if colors == "quit":
+        break
+    colors.append(colors)
+    print(colors)
+    
+else:
+    print("You have entered the following colors: {colors} ")
+    
+    

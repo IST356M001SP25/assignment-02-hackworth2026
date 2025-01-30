94c1d3b9b75649499b1c6176ad29746d8d8c4a84
hackworth2026 <Ahhackwo@syr.edu>
2025-01-30 12:26:01 -0500

    Modifications to Challenege 1 (Assignment 2)

---
+++ Challenges/Challenge1.py
@@ -0,0 +1,18 @@
+attempt = 0
+
+while True:
+    passcode = input("Type in your password:")
+    if passcode == "secret":
+        print("Access Granted")
+
+        break
+
+    else:
+        attempt +=1
+        print(f"The Password {passcode} is incorrect")
+        print(f"Attempt amounts left {5-attempt}")
+
+    if attempt >=5:
+        print("Your account is now locked")
+
+    
